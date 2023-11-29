f4208226fea554d88d200b9066602dfa6af41685
arinming <jyandysys1@naver.com>
2023-11-29 14:29:05 +0900

    [푸] : 7주차 프로젝트 세팅

---
+++ 푸_김아린/spring/src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,32 @@
+package umc.study.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}

e6da211831dd1e5b81e8d37f351556f2a3f3412c
Luke_YS <yjsmk0902@gmail.com>
2023-11-30 21:51:51 +0900

    Feat: Add JoinDto Validation

---
+++ 루크_양승민/루크/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,61 @@
+package umc.spring.validation.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.repository.FoodCategoryRepository;
+import umc.spring.validation.annotation.ExistCategories;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    /**
+     * Initializes the validator in preparation for
+     * {@link #isValid(Object, ConstraintValidatorContext)} calls.
+     * The constraint annotation for a given constraint declaration
+     * is passed.
+     * <p>
+     * This method is guaranteed to be called before any use of this instance for
+     * validation.
+     * <p>
+     * The default implementation is a no-op.
+     *
+     * @param constraintAnnotation annotation instance for a given constraint declaration
+     */
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    /**
+     * Implements the validation logic.
+     * The state of {@code value} must not be altered.
+     * <p>
+     * This method can be accessed concurrently, thread-safety must be ensured
+     * by the implementation.
+     *
+     * @param value   object to validate
+     * @param context context in which the constraint is evaluated
+     * @return {@code false} if {@code value} does not pass the constraint
+     */
+    @Override
+    public boolean isValid(List<Long> value, ConstraintValidatorContext context) {
+        boolean isValid = value.stream()
+                .allMatch(foodCategoryRepository::existsById);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString())
+                    .addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
--- 루크_양승민/루크/src/main/java/umc/spring/web/dto/MemberRequest.java
+++ 루크_양승민/루크/src/main/java/umc/spring/web/dto/MemberRequest.java
@@ -1,21 +1,32 @@
 package umc.spring.web.dto;
 
 import lombok.Getter;
+import umc.spring.validation.annotation.ExistCategories;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class MemberRequest {
 
     @Getter
     public static class JoinDto{
-        String username;
+        @NotBlank
+        String name;
+        @NotNull
         Integer gender;
-        Integer age;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @ExistCategories
         List<Long> preferCategory;
     }
 }

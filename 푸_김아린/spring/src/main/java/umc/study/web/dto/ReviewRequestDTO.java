3c0efc34a3e013127b833df795ab61e2a0509e39
arinming <jyandysys1@naver.com>
2023-11-30 22:08:18 +0900

    [푸] : 9주차 가게에 리뷰 추가하기 API

---
+++ 푸_김아린/spring/src/main/java/umc/study/web/dto/ReviewRequestDTO.java
@@ -0,0 +1,16 @@
+package umc.study.web.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class ReviewRequestDTO {
+    @Getter
+    public static class PostDto {
+        @NotBlank
+        String title;
+        @NotNull
+        Float score;
+    }
+}

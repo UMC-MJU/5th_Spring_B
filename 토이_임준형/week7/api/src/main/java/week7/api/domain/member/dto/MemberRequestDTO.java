64a7fe733bbbba9f90628e18bdd5ed1c40957bf4
toychip <dlawnsgud427@naver.com>
2023-11-30 20:33:57 +0900

    FEAT: Annotation (#4)
    Annotation, Validator 활용

--- 토이_임준형/week7/api/src/main/java/week7/api/domain/member/dto/MemberRequestDTO.java
+++ 토이_임준형/week7/api/src/main/java/week7/api/domain/member/dto/MemberRequestDTO.java
@@ -1,17 +1,28 @@
 package week7.api.domain.member.dto;
 
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
+import week7.api.global.validation.anotation.ExistCategories;
 
 public class MemberRequestDTO {
     @Getter
     public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @ExistCategories
         List<Long> foodCategory;
     }
 }

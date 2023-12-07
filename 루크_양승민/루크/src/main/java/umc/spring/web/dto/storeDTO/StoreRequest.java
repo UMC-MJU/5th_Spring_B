4229870471e288ba63d47fbaeb3697c246e2d8ad
Luke_YS <yjsmk0902@gmail.com>
2023-12-07 15:15:33 +0900

    Refactor: Fix StoreRequest Validate Annotation score 필드 변경

--- 루크_양승민/루크/src/main/java/umc/spring/web/dto/storeDTO/StoreRequest.java
+++ 루크_양승민/루크/src/main/java/umc/spring/web/dto/storeDTO/StoreRequest.java
@@ -3,17 +3,22 @@ package umc.spring.web.dto.storeDTO;
 import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class StoreRequest {
 
     @Getter
     public static class AddDto {
+
         @NotEmpty
         String name;
+
         @NotEmpty
         String address;
-        @NotEmpty
+
+        @Size(max = 5)
         Float score;
+
         @NotEmpty
         Long regionId;
     }

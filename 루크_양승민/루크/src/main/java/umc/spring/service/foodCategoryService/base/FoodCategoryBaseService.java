1ccd07af325a183ffee6084ab10c17675e764279
Luke_YS <yjsmk0902@gmail.com>
2023-12-07 10:12:45 +0900

    Feat: Add FoodCategoryBaseService isFoodCategories 메소드 작성

---
+++ 루크_양승민/루크/src/main/java/umc/spring/service/foodCategoryService/base/FoodCategoryBaseService.java
@@ -0,0 +1,9 @@
+package umc.spring.service.foodCategoryService.base;
+
+import java.util.List;
+
+public interface FoodCategoryBaseService {
+
+    Boolean isFoodCategories(List<Long> value);
+
+}
---
+++ 루크_양승민/루크/src/main/java/umc/spring/service/foodCategoryService/base/FoodCategoryBaseServiceImpl.java
@@ -0,0 +1,20 @@
+package umc.spring.service.foodCategoryService.base;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.spring.repository.FoodCategoryRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FoodCategoryBaseServiceImpl implements FoodCategoryBaseService {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    public Boolean isFoodCategories(List<Long> value){
+        return value.stream().allMatch(foodCategoryRepository::existsById);
+    }
+
+}

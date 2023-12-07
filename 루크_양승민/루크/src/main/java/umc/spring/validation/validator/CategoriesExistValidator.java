49f483c0e0d2fbf0de57accd6301e39548aa9881
Luke_YS <yjsmk0902@gmail.com>
2023-12-07 14:57:56 +0900

    Refactor: Refactor CategoriesExistValidator 레포지토리 계층이 아닌 서비스계층을 불러오도록 재설계

--- 루크_양승민/루크/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
+++ 루크_양승민/루크/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.repository.FoodCategoryRepository;
+import umc.spring.service.foodCategoryService.base.FoodCategoryBaseService;
 import umc.spring.validation.annotation.ExistCategories;
 
 import javax.validation.ConstraintValidator;
@@ -14,12 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
 
-    private final FoodCategoryRepository foodCategoryRepository;
+    private final FoodCategoryBaseService foodCategoryBaseService;
 
     @Override
     public boolean isValid(List<Long> value, ConstraintValidatorContext context) {
-        boolean isValid = value.stream()
-                .allMatch(foodCategoryRepository::existsById);
+        boolean isValid = foodCategoryBaseService.isFoodCategories(value);
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();

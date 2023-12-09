c47a3804030adf716636ee4dea58d4d456904654
Luke_YS <yjsmk0902@gmail.com>
2023-12-09 19:16:21 +0900

    Feat: Add status field to MemberMissionConverter

--- 루크_양승민/루크/src/main/java/umc/spring/converter/mapping/MemberMissionConverter.java
+++ 루크_양승민/루크/src/main/java/umc/spring/converter/mapping/MemberMissionConverter.java
@@ -2,6 +2,7 @@ package umc.spring.converter.mapping;
 
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 
 public class MemberMissionConverter {
@@ -10,6 +11,7 @@ public class MemberMissionConverter {
         return MemberMission.builder()
                 .member(member)
                 .mission(mission)
+                .status(MissionStatus.CHALLENGING)
                 .build();
     }
 }

2c6e1f93d9d4fbd76dab81a2d5886b98a7a9c2f3
toychip <dlawnsgud427@naver.com>
2023-12-11 10:24:27 +0900

    DOCS: Mission 2, 3 Swagger 명세서 작성 (#13)

--- 토이_임준형/week7/api/src/main/java/week7/api/domain/mission/controller/MissionController.java
+++ 토이_임준형/week7/api/src/main/java/week7/api/domain/mission/controller/MissionController.java
@@ -1,6 +1,10 @@
 package week7.api.domain.mission.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,11 +31,25 @@ public class MissionController {
         return ApiResponse.onSuccess(missionService.registerMission(request));
     }
 
+    @Operation(summary = "모든 가게의 미션 목록 조회 API", description = "모든 가게의 미션들을 조회하는 API이며, 선택적으로 특정 스토어의 이름으로 필터링 가능합니다. 페이징을 포함합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK, 성공", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "미션을 찾을 수 없음", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    @Parameter(name = "storeName", description = "가게의 이름, query parameter 입니다.", required = false)
+
     @GetMapping("/missions")
     public ApiResponse<MissionListResponse> getMissions(@RequestParam(required = false) String storeName, Pageable pageable) {
         return ApiResponse.onSuccess(missionService.getMissions(storeName, pageable));
     }
 
+    @Operation(summary = "내 미션 목록 조회 API", description = "현재 로그인한 사용자의 미션들을 조회하는 API이며, 페이징을 포함합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK, 성공", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 필요", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "접근 권한 없음", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
     @GetMapping("/myPage/missions")
     public ApiResponse<MissionListResponse> getMyMissions(Pageable pageable) {
         return ApiResponse.onSuccess(missionService.getMyMissions(pageable));
